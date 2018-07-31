82a8036d77ae00e9fc2549f726500f2773ff0bc5
peachhuang <1107674417@qq.com>
2018-07-31 18:11:27 +0800

    2018.7.30

---
+++ app/src/main/java/com/coolweather/util/HttpUtil.java
@@ -0,0 +1,17 @@
+package com.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by 胡钊 on 2018/7/30.
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new  Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
